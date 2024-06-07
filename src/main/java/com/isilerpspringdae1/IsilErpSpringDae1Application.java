967cb45be2d5a3232623fd07d79585040a475be7
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2024-06-07 15:35:02 -0500

    Initial commit

---
+++ src/main/java/com/isilerpspringdae1/IsilErpSpringDae1Application.java
@@ -0,0 +1,13 @@
+package com.isilerpspringdae1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsilErpSpringDae1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsilErpSpringDae1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/isilerpspringdae1/IsilErpSpringDae1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isilerpspringdae1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsilErpSpringDae1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
