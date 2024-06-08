96a45c776f632223857c389482389d86a5352909
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2024-06-07 23:35:05 -0500

    spring start

--- src/main/java/com/isilerpspringdae1/IsilErpSpringDae1Application.java
+++ src/main/java/com/isilerpspringdae1/IsilErpSpringDae1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IsilErpSpringDae1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IsilErpSpringDae1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IsilErpSpringDae1Application.class, args);
+    }
 
 }
